ab7eda6458e498f233756a3b100b84e77a813319
RMolefe <125550241+RMolefe@users.noreply.github.com>
2023-11-16 12:30:53 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://h6dj16rj6h.execute-api.eu-west-1.amazonaws.com/default/Reneilwe",
+                url: "https://bdp4ubckz3.execute-api.eu-west-2.amazonaws.com/default/ReneilweMolefe",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
